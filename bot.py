7e697eac513ddf606faa89df6b2d30b1843b0184
Lucas B <56450683+lucasbd11@users.noreply.github.com>
2020-06-04 00:11:31 +0200

    add background task for auto refreshing

--- bot.py
+++ bot.py
@@ -1,30 +1,140 @@
 import discord
+from discord.ext import commands
 import copy
+import datetime
+import time
+import asyncio
 
+client = commands.Bot(command_prefix = "!")
 
-client = discord.Client()
 AUTHORIZED_ROLE = ["professeur"] #liste des rôles autorisés à utiliser la commande !appel
 BYPASSED_ROLE = ["professeur"] #liste des rôles qui ne seront jamais comptés comme absents
+saved_data = []
+time_auto_refresh = 60
 REMOVE_MSG_AFTER_CMD = True #condition si le bot supprime le message de avec la commande après celle ci
 TOKEN = "token"
+
+
+
+
+
+
 
 print("Connexion...")
 @client.event
 async def on_ready():
-    print("Bot connecté !")
+    print("Bot connecté ! ",datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-@client.event
-async def on_message(message): #fonction executée lorsqu'il y a un nouveau message (à optimiser)
+
+async def auto_refresh():
+    await client.wait_until_ready()
     
-    if message.content.startswith("!appel") and not(message.author.bot): #vérification que c'est la commande et que la personne n'est pas un bot (sécurité)
+    global saved_data
+    while not(client.is_closed()):
+        
+        to_remove = []
+        for data in saved_data:
+            
+
+                
+            message = data["command_msg"]
+            
+            
+            TARGETED_ROLE = ["@everyone"]
+    
+            if len(message.content)>6:
+                TARGETED_ROLE = message.content[7:].split(",")
+            
+            list_user_server = message.author.guild.members 
+            
+            list_user_online = [i for i in message.author.voice.channel.members] 
+            
+            for i in list_user_online: 
+                list_user_server.pop(list_user_server.index(i))
+            
+            list_user_not_online = copy.copy(list_user_server) 
+            
+            mess = ""
+            offline_count = 0
+            
+            for x in range(len(list_user_not_online)):
+                
+                temp_user_roles = [i.name for i in list_user_not_online[x].roles] 
+
+                if len(TARGETED_ROLE) > 1:
+                    is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (min([i in temp_user_roles[1:] for i in TARGETED_ROLE]))
+                else:
+                    is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (any((i in TARGETED_ROLE for i in temp_user_roles)))
+
+
+                
+                if not(list_user_not_online[x].bot) and is_targeted_role:
+                
+                    if list_user_not_online[x].nick == None:
+                        name = str(list_user_not_online[x])[:str(list_user_not_online[x]).find("#")]
+                    else:
+                        name = list_user_not_online[x].nick 
+                    offline_count += 1 
+                    mess += "-" + name + " ("+ list_user_not_online[x].mention +")" + "\n" 
+            
+            if mess != "":
+
+                if len(TARGETED_ROLE) == 1 and TARGETED_ROLE[0] != "@everyone":
+                    if offline_count == 1:
+                        mess = f"Il y a {offline_count} personne du groupe {TARGETED_ROLE[0]} absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                    else:
+                        mess = f"Il y a {offline_count} personne(s) du groupe {TARGETED_ROLE[0]} absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess 
+                else:
+                    if offline_count == 1:
+                        mess = f"Il y a {offline_count} personne absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                    else:
+                        mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                
+                
+            
+            else:
+                mess = "Tout le monde est présent."
+            
+            if data["cycle"] < time_auto_refresh-1:
+                cycle_temp = data["cycle"]%4
+                if cycle_temp == 0:
+                    mess += "\n\n*Actualisation automatique*    ⌛"
+                elif cycle_temp == 1:
+                    mess += "\n\n*Actualisation automatique.*   ⌛"
+                elif cycle_temp == 2:
+                    mess += "\n\n*Actualisation automatique..*  ⌛"
+                elif cycle_temp == 3:
+                    mess += "\n\n*Actualisation automatique...* ⌛"
+            
+            data["cycle"] += 1
+            await data["bot_message"].edit(content=mess)
+        
+            if data["cycle"] >= time_auto_refresh:
+                print(f'Data removed:\nGuide: {data["command_msg"].guild.name}\nProf: {data["command_msg"].author.name}\n------')
+                to_remove += [data]
+            
+        for i in to_remove:
+            saved_data.pop(saved_data.index(i))
+        await asyncio.sleep(1)
+        
+        
+        
+
+@client.command(aliases = ["ap"])
+async def appel(ctx,*param):
+    message = ctx.message
+    if not(message.author.bot): #vérification que c'est la commande et que la personne n'est pas un bot (sécurité)
 
         TARGETED_ROLE = ["@everyone"] #rôles dont le bot doit verifier la présence des personnes possédant (un des rôles) dans le salon
 
         if len(message.content)>6:
             TARGETED_ROLE = message.content[7:].split(",") #on met un seul rôle ciblé si il est passé en paramètre
-        print(TARGETED_ROLE)
+
 
         if any(n in AUTHORIZED_ROLE for n in [i.name for i in message.author.roles]): #vérification que l'auteur du message possède le rôle autorisé
+        
+            global saved_data
+            
             if message.author.voice != None: #vérification que l'auteur du message est dans un salon vocal
                 list_user_server = message.author.guild.members #création de la liste des membres du serveur
                 
@@ -48,7 +158,9 @@ async def on_message(message): #fonction executée lorsqu'il y a un nouveau mess
                         is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (min([i in temp_user_roles[1:] for i in TARGETED_ROLE]))
                     else:
                         is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (any((i in TARGETED_ROLE for i in temp_user_roles)))
-             
+
+
+                    
                     if not(list_user_not_online[x].bot) and is_targeted_role: #condition pour voir si la personne est ciblée et si elle n'est pas un bot
                     #if is_targeted_role:
                     
@@ -64,11 +176,25 @@ async def on_message(message): #fonction executée lorsqu'il y a un nouveau mess
                 if mess != "": #vérification qu'il y a bien des personnes absentes (sinon la chaîne de caractère n'est pas modifiée)
                     
                     
+                    #ajout de l'entête du message
                     if len(TARGETED_ROLE) == 1 and TARGETED_ROLE[0] != "@everyone":
-                        mess = f"Il y a {offline_count} personne(s) du groupe {TARGETED_ROLE[0]} absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess #ajout de l'entête du message
+                        if offline_count == 1:
+                            mess = f"Il y a {offline_count} personne du groupe {TARGETED_ROLE[0]} absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                        else:
+                            mess = f"Il y a {offline_count} personne(s) du groupe {TARGETED_ROLE[0]} absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess 
                     else:
-                        mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess #ajout de l'entête du message
-                    await message.channel.send(mess) #coroutine pour envoyer le message
+                        if offline_count == 1:
+                            mess = f"Il y a {offline_count} personne absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                        else:
+                            mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                    mess += "\n\n*Actualisation automatique...* ⌛"
+                    
+                    
+                    
+                    bot_message = await message.channel.send(mess) #coroutine pour envoyer le message
+                    
+                    data_dict = {"bot_message":bot_message,"cycle":0,"time":time.time(),"command_msg":message}
+                    saved_data += [data_dict]
                 
                 else:
                     await message.channel.send("Tout le monde est présent.") #un message est envoyé si tout le monde est présent
@@ -78,8 +204,11 @@ async def on_message(message): #fonction executée lorsqu'il y a un nouveau mess
             
         else:
             await message.channel.send("Vous n'avez pas la premission d'utiliser cette commande.") #un message est envoyé si la personne n'a pas la permission d'utiliser la commande
+        
         if REMOVE_MSG_AFTER_CMD:
             await message.delete() #on supprime le message envoyé par la personne
         
 
+
+client.loop.create_task(auto_refresh())
 client.run(TOKEN)
