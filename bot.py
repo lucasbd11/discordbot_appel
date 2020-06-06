0015ab4cf65c432351c9a906d29a35cec51d4a25
Lucas B <56450683+lucasbd11@users.noreply.github.com>
2020-06-06 20:01:20 +0200

    resolve bug with removed messages

--- bot.py
+++ bot.py
@@ -20,10 +20,11 @@ TOKEN = "token"
 
 
 
+
 print("Connexion...")
 @client.event
 async def on_ready():
-    print("Bot connecté ! ",datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    print("Bot connecté ! ",datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")," UTC")
 
 
 async def auto_refresh():
@@ -36,81 +37,83 @@ async def auto_refresh():
         for data in saved_data:
             
 
+            try:
+                message = data["command_msg"]
+                channel_user = data["voice_channel"]
                 
-            message = data["command_msg"]
-            
-            
-            TARGETED_ROLE = ["@everyone"]
+                
+                
+                TARGETED_ROLE = ["@everyone"]
+        
+                if len(message.content)>6:
+                    TARGETED_ROLE = message.content[7:].split(",")
+                
+                list_user_server = message.author.guild.members 
+                
+                list_user_online = [i for i in channel_user.members] 
+                
+                for i in list_user_online: 
+                    list_user_server.pop(list_user_server.index(i))
+                
+                list_user_not_online = copy.copy(list_user_server) 
+                
+                mess = ""
+                offline_count = 0
+                
+                for x in range(len(list_user_not_online)):
+                    
+                    temp_user_roles = [i.name for i in list_user_not_online[x].roles] 
     
-            if len(message.content)>6:
-                TARGETED_ROLE = message.content[7:].split(",")
-            
-            list_user_server = message.author.guild.members 
-            
-            list_user_online = [i for i in message.author.voice.channel.members] 
-            
-            for i in list_user_online: 
-                list_user_server.pop(list_user_server.index(i))
-            
-            list_user_not_online = copy.copy(list_user_server) 
-            
-            mess = ""
-            offline_count = 0
-            
-            for x in range(len(list_user_not_online)):
+                    if len(TARGETED_ROLE) > 1:
+                        is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (min([i in temp_user_roles[1:] for i in TARGETED_ROLE]))
+                    else:
+                        is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (any((i in TARGETED_ROLE for i in temp_user_roles)))
+    
+    
+                    
+                    if not(list_user_not_online[x].bot) and is_targeted_role:
+                    
+                        if list_user_not_online[x].nick == None:
+                            name = str(list_user_not_online[x])[:str(list_user_not_online[x]).find("#")]
+                        else:
+                            name = list_user_not_online[x].nick 
+                        offline_count += 1 
+                        mess += "-" + name + " ("+ list_user_not_online[x].mention +")" + "\n" 
                 
-                temp_user_roles = [i.name for i in list_user_not_online[x].roles] 
-
-                if len(TARGETED_ROLE) > 1:
-                    is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (min([i in temp_user_roles[1:] for i in TARGETED_ROLE]))
+                if mess != "":
+    
+                    if len(TARGETED_ROLE) == 1 and TARGETED_ROLE[0] != "@everyone":
+                        if offline_count == 1:
+                            mess = f"Il y a {offline_count} personne du groupe {TARGETED_ROLE[0]} absente dans le salon {channel_user.name}:\n\n" + mess
+                        else:
+                            mess = f"Il y a {offline_count} personne(s) du groupe {TARGETED_ROLE[0]} absente(s) dans le salon {channel_user.name}:\n\n" + mess 
+                    else:
+                        if offline_count == 1:
+                            mess = f"Il y a {offline_count} personne absente dans le salon {channel_user.name}:\n\n" + mess
+                        else:
+                            mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {channel_user.name}:\n\n" + mess
+                    
+                    
+                
                 else:
-                    is_targeted_role = not(any(i in BYPASSED_ROLE for i in temp_user_roles)) and (any((i in TARGETED_ROLE for i in temp_user_roles)))
-
-
+                    mess = "Tout le monde est présent."
                 
-                if not(list_user_not_online[x].bot) and is_targeted_role:
-                
-                    if list_user_not_online[x].nick == None:
-                        name = str(list_user_not_online[x])[:str(list_user_not_online[x]).find("#")]
-                    else:
-                        name = list_user_not_online[x].nick 
-                    offline_count += 1 
-                    mess += "-" + name + " ("+ list_user_not_online[x].mention +")" + "\n" 
-            
-            if mess != "":
-
-                if len(TARGETED_ROLE) == 1 and TARGETED_ROLE[0] != "@everyone":
-                    if offline_count == 1:
-                        mess = f"Il y a {offline_count} personne du groupe {TARGETED_ROLE[0]} absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
-                    else:
-                        mess = f"Il y a {offline_count} personne(s) du groupe {TARGETED_ROLE[0]} absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess 
-                else:
-                    if offline_count == 1:
-                        mess = f"Il y a {offline_count} personne absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
-                    else:
-                        mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess
+                if data["cycle"] < time_auto_refresh-1:
+                    cycle_temp = data["cycle"]%2
+                    if cycle_temp == 0:
+                        mess += "\n\n*Actualisation automatique*   ⏳"
+                    elif cycle_temp == 1:
+                        mess += "\n\n*Actualisation automatique*   ⌛"
                 
                 
-            
-            else:
-                mess = "Tout le monde est présent."
-            
-            if data["cycle"] < time_auto_refresh-1:
-                cycle_temp = data["cycle"]%4
-                if cycle_temp == 0:
-                    mess += "\n\n*Actualisation automatique*    ⌛"
-                elif cycle_temp == 1:
-                    mess += "\n\n*Actualisation automatique.*   ⌛"
-                elif cycle_temp == 2:
-                    mess += "\n\n*Actualisation automatique..*  ⌛"
-                elif cycle_temp == 3:
-                    mess += "\n\n*Actualisation automatique...* ⌛"
+                await data["bot_message"].edit(content=mess)
+            except:
+                print("Erreur:\nDate:",str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + " UTC","\nMessage:",message,"\nChannel:",channel_user)
             
             data["cycle"] += 1
-            await data["bot_message"].edit(content=mess)
-        
             if data["cycle"] >= time_auto_refresh:
-                print(f'Data removed:\nGuide: {data["command_msg"].guild.name}\nProf: {data["command_msg"].author.name}\n------')
+                date_now = str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")) + " UTC"
+                print(f'Data removed:\nDate: {date_now}\nGuilde: {data["command_msg"].guild.name}\nProf: {data["command_msg"].author.name}\n------')
                 to_remove += [data]
             
         for i in to_remove:
@@ -187,13 +190,13 @@ async def appel(ctx,*param):
                             mess = f"Il y a {offline_count} personne absente dans le salon {message.author.voice.channel.name}:\n\n" + mess
                         else:
                             mess = f"Il y a {offline_count} personne(s) absente(s) dans le salon {message.author.voice.channel.name}:\n\n" + mess
-                    mess += "\n\n*Actualisation automatique...* ⌛"
+                    mess += "\n\n*Actualisation automatique*   ⌛"
                     
                     
                     
                     bot_message = await message.channel.send(mess) #coroutine pour envoyer le message
-                    
-                    data_dict = {"bot_message":bot_message,"cycle":0,"time":time.time(),"command_msg":message}
+
+                    data_dict = {"bot_message":bot_message,"cycle":0,"time":time.time(),"command_msg":message,"voice_channel":message.author.voice.channel}
                     saved_data += [data_dict]
                 
                 else:
@@ -212,3 +215,4 @@ async def appel(ctx,*param):
 
 client.loop.create_task(auto_refresh())
 client.run(TOKEN)
+
